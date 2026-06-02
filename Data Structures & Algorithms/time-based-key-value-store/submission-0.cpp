04dc0bc87efd91f3b8dbefd0350152c9dd287aed
Kartikey Joshi <113437243+aidendorian@users.noreply.github.com>
2026-06-02 16:03:25 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,28 @@
+class TimeMap {
+    unordered_map <string, vector<pair<int, string>>> hash;
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        hash[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        string ans = "";
+        auto& v = hash[key];
+        int l = 0;
+        int r = v.size() - 1;
+        int mid = 0;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(hash[key][mid].first <= timestamp){
+                ans = hash[key][mid].second;
+                l = mid + 1;
+            }
+            else
+                r = mid-1;
+        }
+        return ans;
+    }
+};
